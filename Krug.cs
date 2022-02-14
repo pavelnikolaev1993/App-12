70dc4ad89e895e43cc28c62aa853a57a9d5bafa8
НиколаевПА <@NIKOLAEV>
2022-02-14 11:11:30 +0300

    Добавьте файлы проекта.

---
+++ Krug.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие_12
+{
+    static class Krug
+    {
+        static public double GetLenght(double R)
+        {
+            return 2 * Math.PI * R;        
+        }
+        static public double GetSquare(double R)
+        {
+            return Math.PI * Math.Pow(R, 2);
+        }
+        static public string GetPoint(double R, double x0, double y0, double x, double y)
+        {
+            double x1 = x0 + R;
+            double y1 = y0 + R;
+            if (x1 > x || y1 > y)
+            {
+                return "Точка принадлежит окружности";
+            }
+            else
+            {
+                return "Точка не принадлежит окружности";
+            }
+           
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Занятие_12;
+Console.WriteLine("Введите радиус окружности:");
+double R = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты центра окружности (x0,y0):");
+double x0 = Convert.ToDouble(Console.ReadLine());
+double y0 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точки (x,y):");
+double x = Convert.ToDouble(Console.ReadLine());
+double y = Convert.ToDouble(Console.ReadLine());
+double square = Krug.GetSquare(R);
+double length = Krug.GetLenght(R);
+string point = Krug.GetPoint(R, x0, y0, x, y);
+Console.WriteLine($"Длина круга {length} \nПлощадь круга {square}\n{point}");
+    
